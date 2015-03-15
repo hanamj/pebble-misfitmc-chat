d72589db72a0f87a5561a436d346a8efab8111c9
John Hanam <hanamj@users.noreply.github.com>
2015-03-15 12:33:09 -0400

    Small text

--- src/app.js
+++ src/app.js
@@ -1,14 +1,40 @@
 var UI = require('ui');
-var helper = require('helpers');
+var chats = require('chats');
 
-var chatCard = new UI.Card({
-  title:'Chats',
-  body: "Loading...",
-  scrollable: true
-});
-chatCard.on('click', 'select', function() {
-  helper.refreshChats(chatCard);
+var mainMenu = new UI.Menu({
+  sections: [{
+    title: 'Misfit Minecraft',
+    items: [{
+      title: 'Online Players',
+      subtitle: 'See who is online.',
+      icon: 'images/pb_mc_creep.png'
+    }, {
+      title: 'Live Chat',
+      subtitle: 'Refresh with Select',
+      icon: 'images/pb_mc_creep.png'
+    }]
+  }]
 });
 
-chatCard.show();
-helper.refreshChats(chatCard);
+mainMenu.show();
+
+mainMenu.on('select', function(e) {
+  if (e.itemIndex === 0) {
+    //Open Players
+  } else if (e.itemIndex === 1) {
+    //Open Chats
+    var chatCard = new UI.Card({
+      title:'Chats',
+      body: "Loading...",
+      scrollable: true,
+      style: "small"
+    });
+    chatCard.on('click', 'select', function() {
+      chats.refreshChats(chatCard);
+    });
+    
+    chatCard.show();
+    chats.refreshChats(chatCard);
+  }
+});
+
